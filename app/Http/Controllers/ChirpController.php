c5416b7875b52ad762a02213efe5c3a6e5c8100d
LeinadCybre1024 <leinadcybre@gmail.com>
2023-09-09 20:48:52 +0300

    CRUD features present

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps', [
+            //
+        ]);
+    }
+}
